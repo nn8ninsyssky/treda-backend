fdff948222b4c519c7eac486764b3c24799e33ce
nn8ninsyssky <nn8ninsyssky@gmail.com>
2026-05-05 20:21:29 +0530

    configuring git setup

--- src/controllers/complaint.controller.js
+++ src/controllers/complaint.controller.js
@@ -242,80 +242,24 @@ exports.getComplaintByDeviceQR = async (req, res, next) => {
 //update complaint status by vendor or technician
 
 
-exports.updateComplaintStatus = async (req, res, next) => {
-  try {
-    const { complaint_id, complaint_status } = req.body;
-
-    const result = await callSP(
-      `SELECT sp_update_complaint_status(
-        :user_id,
-        :complaint_id,
-        :complaint_status
-      )`,
-      {
-        user_id: req.user.id,
-        complaint_id,
-        complaint_status
-      }
-    );
-
-    const response = result?.[0]?.sp_update_complaint_status;
-
-    if (!response.success) {
-      return res.status(400).json(response);
-    }
-
-    return res.status(200).json(response);
-
-  } catch (err) {
-    next(err);
-  }
-};
-
-
 // exports.updateComplaintStatus = async (req, res, next) => {
 //   try {
-//     const {
-//       complaint_no,
-//       complaint_status = null,
-//       vendor_assignment_status = null,
-//       technician_assignment_status = null,
-//     } = req.body;
-
-//     if (!complaint_no) {
-//       return res.status(400).json({
-//         success: false,
-//         message: "complaint_no is required",
-//       });
-//     }
+//     const { complaint_id, complaint_status } = req.body;
 
 //     const result = await callSP(
-//       `
-//       SELECT public.sp_update_complaint_status(
+//       `SELECT sp_update_complaint_status(
 //         :user_id,
-//         :complaint_no,
-//         :complaint_status,
-//         :vendor_assignment_status,
-//         :technician_assignment_status
-//       )
-//       `,
+//         :complaint_id,
+//         :complaint_status
+//       )`,
 //       {
 //         user_id: req.user.id,
-//         complaint_no,
-//         complaint_status,
-//         vendor_assignment_status,
-//         technician_assignment_status,
+//         complaint_id,
+//         complaint_status
 //       }
 //     );
 
 //     const response = result?.[0]?.sp_update_complaint_status;
-
-//     if (!response) {
-//       return res.status(500).json({
-//         success: false,
-//         message: "Failed to update complaint status",
-//       });
-//     }
 
 //     if (!response.success) {
 //       return res.status(400).json(response);
@@ -327,6 +271,62 @@ exports.updateComplaintStatus = async (req, res, next) => {
 //     next(err);
 //   }
 // };
+
+
+exports.updateComplaintStatus = async (req, res, next) => {
+  try {
+    const {
+      complaint_no,
+      complaint_status = null,
+      vendor_assignment_status = null,
+      technician_assignment_status = null,
+    } = req.body;
+
+    if (!complaint_no) {
+      return res.status(400).json({
+        success: false,
+        message: "complaint_no is required",
+      });
+    }
+
+    const result = await callSP(
+      `
+      SELECT public.sp_update_complaint_status(
+        :user_id,
+        :complaint_no,
+        :complaint_status,
+        :vendor_assignment_status,
+        :technician_assignment_status
+      )
+      `,
+      {
+        user_id: req.user.id,
+        complaint_no,
+        complaint_status,
+        vendor_assignment_status,
+        technician_assignment_status,
+      }
+    );
+
+    const response = result?.[0]?.sp_update_complaint_status;
+
+    if (!response) {
+      return res.status(500).json({
+        success: false,
+        message: "Failed to update complaint status",
+      });
+    }
+
+    if (!response.success) {
+      return res.status(400).json(response);
+    }
+
+    return res.status(200).json(response);
+
+  } catch (err) {
+    next(err);
+  }
+};
 // get all complaint by admin vendor and technician
 
 exports.getAllComplaintsRolewise = async (req, res, next) => {
